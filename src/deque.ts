49accc39df2285e8a9f54713954d32cbb93fe8d1
vitkarpov <viktor.karpov@toptal.com>
2019-05-12 20:14:21 +0300

    feat(deque): Implements pop

--- src/deque.ts
+++ src/deque.ts
@@ -19,8 +19,24 @@ export class Deque<T> {
       this.head = this.tail = next;
     } else {
       this.tail.next = next;
+      next.prev = this.tail;
       this.tail = this.tail.next;
     }
     return this.tail.val;
+  }
+  pop(): T {
+    if (!this.tail) {
+      throw new RangeError('Cannot pop. Your container is empty.');
+    }
+    if (this.head === this.tail) {
+      const val = this.tail.val;
+      this.head = this.tail = undefined;
+      return val;
+    }
+    const val = this.tail.val;
+    const prev = this.tail.prev as Node<T>;
+    prev.next = undefined;
+    this.tail = prev;
+    return val;
   }
 }
--- tests/deque.test.ts
+++ tests/deque.test.ts
@@ -1,47 +1,92 @@
 import { Deque } from '../index';
 
 describe('Deque', () => {
-  const tests = [
-    {
-      /**
-       * Put here Deque.prototype methods
-       * which is called in this particular order during the test.
-       */
-      methods: [Deque.prototype.push],
-      /**
-       * Put here arguments for methods specified above.
-       */
-      args: [1],
-      /**
-       * Define expect function which takes deque.
-       * You might check some state using jest's expect.
-       */
-      expect: (deque: Deque<number>) => {
-        expect(deque.head && deque.head.val).toEqual(1);
-        expect(deque.tail && deque.tail.val).toEqual(1);
+  describe('push & pop scenarious', () => {
+    const tests = [
+      {
+        /**
+         * Put here Deque.prototype methods
+         * which is called in this particular order during the test.
+         */
+        methods: [Deque.prototype.push],
+        /**
+         * Put here arguments for methods specified above.
+         */
+        args: [1],
+        /**
+         * Define expect function which takes deque.
+         * You might check some state using jest's expect.
+         */
+        expect: (deque: Deque<number>) => {
+          expect(deque.head && deque.head.val).toEqual(1);
+          expect(deque.tail && deque.tail.val).toEqual(1);
+        },
       },
-    },
-    {
-      methods: [
-        Deque.prototype.push,
-        Deque.prototype.push,
-        Deque.prototype.push,
-      ],
-      args: [1, 2, 3],
-      expect: (deque: Deque<number>) => {
-        expect(deque.head && deque.head.val).toEqual(1);
-        expect(deque.tail && deque.tail.val).toEqual(3);
+      {
+        methods: [
+          Deque.prototype.push,
+          Deque.prototype.push,
+          Deque.prototype.push,
+        ],
+        args: [1, 2, 3],
+        expect: (deque: Deque<number>) => {
+          expect(deque.head && deque.head.val).toEqual(1);
+          expect(deque.tail && deque.tail.val).toEqual(3);
+        },
       },
-    },
-  ];
-  tests.forEach(({ methods, args, expect }, i) => {
-    it(`test #${i}`, () => {
+      {
+        methods: [
+          Deque.prototype.push,
+          Deque.prototype.push,
+          Deque.prototype.push,
+          Deque.prototype.pop,
+          Deque.prototype.pop,
+        ],
+        args: [1, 2, 3, null, null],
+        expect: (deque: Deque<number>) => {
+          expect(deque.head && deque.head.val).toEqual(1);
+          expect(deque.tail && deque.tail.val).toEqual(1);
+        },
+      },
+      {
+        methods: [
+          Deque.prototype.push,
+          Deque.prototype.push,
+          Deque.prototype.pop,
+          Deque.prototype.push,
+          Deque.prototype.pop,
+          Deque.prototype.push,
+        ],
+        args: [1, 2, null, 3, null, 4],
+        expect: (deque: Deque<number>) => {
+          expect(deque.head && deque.head.val).toEqual(1);
+          expect(deque.tail && deque.tail.val).toEqual(4);
+        },
+      },
+    ];
+    tests.forEach(({ methods, args, expect }, i) => {
+      it(`test #${i}`, () => {
+        const deque = new Deque<number>();
+
+        methods.forEach((method, i) => {
+          // pop doesn't take any arguments
+          if (method === Deque.prototype.pop) {
+            deque.pop();
+          } else {
+            method.call(deque, args[i]);
+          }
+        });
+        expect(deque);
+      });
+    });
+  });
+  describe('#pop', () => {
+    it('throws an error called on empty container', () => {
       const deque = new Deque<number>();
 
-      methods.forEach((method, i) => {
-        method.call(deque, args[i]);
-      });
-      expect(deque);
+      expect(() => deque.pop()).toThrowError(
+        'Cannot pop. Your container is empty.'
+      );
     });
   });
 });
