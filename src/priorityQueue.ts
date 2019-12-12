acff4877b7808a93900c39c44046c7ed54b1b659
vitkarpov <viktor.karpov@toptal.com>
2019-12-12 17:13:30 +0300

    feat(heaps): PriorityQueue

---
+++ src/priorityQueue.ts
@@ -0,0 +1,57 @@
+export class PriorityQueue<T> {
+  constructor(comparator: (a: T, b: T) => boolean) {
+    this.comparator = comparator;
+  }
+
+  top() {
+    return this.q[0];
+  }
+
+  push(val: T) {
+    this.q.push(val);
+    this.bubbleUp();
+  }
+
+  pop() {
+    if (this.q.length === 0) {
+      throw new RangeError('Cannot pop. Your container is empty.');
+    }
+    this.q[0] = this.q.pop() as T;
+    this.sinkDown(0);
+  }
+
+  private q: T[] = [];
+  private comparator: (a: T, b: T) => boolean;
+  private bubbleUp() {
+    let curr = this.q.length - 1;
+
+    while (curr > 0) {
+      const next = Math.floor((curr - 1) / 2);
+      const val = this.q[curr];
+      const parent = this.q[next];
+
+      if (this.comparator(parent, val)) {
+        break;
+      }
+      this.q[curr] = parent;
+      this.q[next] = val;
+      curr = next;
+    }
+  }
+  private sinkDown(max: number) {
+    const left = 2 * max + 1;
+    const right = 2 * max + 2;
+    let curr = max;
+
+    if (left <= this.q.length && this.q[left] > this.q[curr]) {
+      curr = left;
+    }
+    if (right <= this.q.length && this.q[right] > this.q[curr]) {
+      curr = right;
+    }
+    if (curr !== max) {
+      [this.q[curr], this.q[max]] = [this.q[max], this.q[curr]];
+      this.sinkDown(curr);
+    }
+  }
+}
